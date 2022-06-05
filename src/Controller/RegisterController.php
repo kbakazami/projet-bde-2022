0ab3fd7ffa05d5d469bd6e656edbf92ba5c82f8c
Kba <struys.wyvin@gmail.com>
2022-06-05 19:14:27 +0200

    register

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,16 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Entity\User;
 use App\Utils\Form;
 use App\Form\LoginType;
 use App\Routing\Attribute\Route;
 use App\Repository\UserRepository;
 use App\Session\Session;
+use DateTime;
+
 
 class RegisterController extends AbstractController
 {
     #[Route(path: "/register", name: "register")]
-    public function login()
+    public function register()
     {
         $errors = [];
 
@@ -31,6 +35,37 @@ class RegisterController extends AbstractController
         echo $this->twig->render('register/register.html.twig', [
             "formulaire" => $formulaire
         ]);
+    }
+
+    /**
+     * @throws \Twig\Error\SyntaxError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\LoaderError
+     */
+    #[Route(path: "/register-add", name: "register-add", httpMethod: "POST")]
+    public function registerAdd(UserRepository $userRepository)
+    {
+
+        //var_dump($_POST);
+
+        $user = $_POST;
+        $user = new User();
+        $user->setFirstName($_POST['prenom'])
+            ->setLastName($_POST['nom'])
+            ->setEmail($_POST['mail'])
+            ->setPassword($_POST['password'])
+            ->setBirthDate(new DateTime($_POST['date']))
+            ->setImage("");
+
+
+        $userRepository->save($user);
+
+        echo $this->twig->render('register/register.html.twig');
+
+    }
+
+    private function setImage(string $string)
+    {
     }
 
 }
