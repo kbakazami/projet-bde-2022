34eff033aeccbac3cc256b30d0ba67971936ebde
Sofia CHAUDHRY <sofia.chaudhry2606@gmail.com>
2022-06-09 23:14:03 +0200

    Responsive login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ["./templates/**/*.html.twig"],
+  content: [
+      "./src/**/*.{html,js}",
+    './templates/**/*.html.twig',
+    './node_modules/tw-elements/dist/js/**/*.js'
+  ],
   theme: {
     screens: {
       'sm': '640px',
@@ -11,12 +15,30 @@ module.exports = {
     extend: {
       colors:{
         'blue': '#2D27FF',
+        'blue-60' : 'rgba(45,39,255, 0.6)',
         'purple': '#AE20D5',
+        'purple-60': 'rgba(174,32,213, 0.6)',
         'pink': '#FF0A6C',
+        'pink-60': 'rgba(255,10,108, 0.6)',
       },
       fontFamily:{
         hind:["hind","roboto"],
         expletus:["expletus","roboto"],
+      },
+      width:{
+        '400':'400px',
+        '300':'300px',
+        '220':'220px'
+      },
+      height:{
+        '400':'400px',
+        '300':'300px',
+      },
+      boxShadow:{
+        'glowEffect':'inset 0 0 10px blue, inset 0 0 40px pink, 0 0 20px purple'
+      },
+      rotate:{
+        '270':'270deg',
       }
     },
   },
@@ -25,5 +47,4 @@ module.exports = {
       display: ['after','before']
     }
   },
-  plugins: [],
 }
