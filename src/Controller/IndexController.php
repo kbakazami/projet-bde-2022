e5889a7a71b40b0ed72a8ef9079bad29d76254cf
Wivin Struys <struys.wyvin@gmail.com>
2022-06-21 10:17:52 +0200

    IndexController event & news to view

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,15 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\EventRepository;
+use App\Repository\NewsRepository;
 use App\Routing\Attribute\Route;
 
 
 class IndexController extends AbstractController
 {
     #[Route(path: "/")]
-    public function index()
+    public function index(EventRepository $eventRepository, NewsRepository $newsRepository)
     {
-        echo $this->twig->render('index/home.html.twig');
+        $events = $eventRepository->findAllEvent();
+
+        $news = $newsRepository->findAllNews();
+
+        echo $this->twig->render('index/home.html.twig', [
+            'events' => $events,
+            'news' => $news
+        ]);
     }
 
 
