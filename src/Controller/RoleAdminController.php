d2534005b3da5f5abba9bf86b30edbb21de88859
Wivin Struys <struys.wyvin@gmail.com>
2022-06-21 08:58:49 +0200

    Access Crud Role done

--- src/Controller/RoleAdminController.php
+++ src/Controller/RoleAdminController.php
@@ -13,87 +13,137 @@ class RoleAdminController extends AbstractController
     #[Route(path: "/admin/form-create-role", name: "create-form-role")]
     public function createFormRole()
     {
-        echo $this->twig->render('admin/role/form-role.html.twig');
+
+        if (!isset($_SESSION['userRole'])) {
+            header('Location: /form-login');
+        }
+        if ($_SESSION['userRole'] !== 'Admin') {
+            echo $this->twig->render('/access.html.twig');
+        } else {
+            echo $this->twig->render('admin/role/form-role.html.twig');
+        }
+
     }
 
     #[Route(path: "/admin/create-role", name: "create-role", httpMethod: "POST")]
     public function createRole(RoleRepository $roleRepository)
     {
-        $validator = new Validator($_POST);
 
-        $validator->required("title")
-            ->length("title", 2, 250);
-
-
-        if ($validator->isValid()) {
-            $role = new Role();
-            $role->setTitle($_POST['title']);
-
-
-            $roleRepository->save($role);
+        if (!isset($_SESSION['userRole'])) {
+            header('Location: /form-login');
         }
-        $errors = $validator->getErrors();
-
-        if(!empty($errors)) {
-            echo $this->twig->render('admin/role/form-role.html.twig', [
-                'errors' => $errors,
-            ]);
+        if ($_SESSION['userRole'] !== 'Admin') {
+            echo $this->twig->render('/access.html.twig');
         } else {
-            header('Location: /admin/list-role');
+
+
+            $validator = new Validator($_POST);
+
+            $validator->required("title")
+                ->length("title", 2, 250);
+
+
+            if ($validator->isValid()) {
+                $role = new Role();
+                $role->setTitle($_POST['title']);
+
+
+                $roleRepository->save($role);
+            }
+            $errors = $validator->getErrors();
+
+            if (!empty($errors)) {
+                echo $this->twig->render('admin/role/form-role.html.twig', [
+                    'errors' => $errors,
+                ]);
+            } else {
+                header('Location: /admin/list-role');
+            }
         }
     }
 
     #[Route(path: "/admin/list-role", name: "list-role")]
     public function listRole(RoleRepository $roleRepository)
     {
-        $roles = $roleRepository->findAllRole();
+        if (!isset($_SESSION['userRole'])) {
+            header('Location: /form-login');
+        }
+        if ($_SESSION['userRole'] !== 'Admin') {
+            echo $this->twig->render('/access.html.twig');
+        } else {
 
-        echo $this->twig->render('admin/role/list-role.html.twig', [
-            'roles' => $roles
-        ]);
+            $roles = $roleRepository->findAllRole();
+
+            echo $this->twig->render('admin/role/list-role.html.twig', [
+                'roles' => $roles
+            ]);
+        }
     }
 
     #[Route(path: "/admin/form-update-role/{id}", name: "form-update-role")]
     public function formUpdateRole(RoleRepository $roleRepository,int $id)
     {
-        $role = $roleRepository->findRoleById($id);
+        if (!isset($_SESSION['userRole'])) {
+            header('Location: /form-login');
+        }
+        if ($_SESSION['userRole'] !== 'Admin') {
+            echo $this->twig->render('/access.html.twig');
+        } else {
 
-        echo $this->twig->render('admin/role/edit-role.html.twig', [
-            'role' => $role
-        ]);
+            $role = $roleRepository->findRoleById($id);
+
+            echo $this->twig->render('admin/role/edit-role.html.twig', [
+                'role' => $role
+            ]);
+        }
     }
 
     #[Route(path: "/admin/update-role/{id}", name: "update-role", httpMethod: "POST")]
     public function updateRole(RoleRepository $roleRepository, int $id)
     {
-        $role = $roleRepository->findRoleById($id);
-        $validator = new Validator($_POST);
-
-        $validator->required("title")
-            ->length("title", 2, 250);
-
-        if ($validator->isValid()) {
-            $roleUpdate = new Role();
-            $roleUpdate->setTitle($_POST['title']);
-
-            $roleRepository->updateRole($roleUpdate, $id);
+        if (!isset($_SESSION['userRole'])) {
+            header('Location: /form-login');
         }
-        $errors = $validator->getErrors();
-
-        if(!empty($errors)) {
-            echo $this->twig->render('admin/role/edit-role.html.twig', [
-                'errors' => $errors,
-                'role' => $role
-            ]);
+        if ($_SESSION['userRole'] !== 'Admin') {
+            echo $this->twig->render('/access.html.twig');
         } else {
-            header('Location: /admin/list-role');
+
+            $role = $roleRepository->findRoleById($id);
+            $validator = new Validator($_POST);
+
+            $validator->required("title")
+                ->length("title", 2, 250);
+
+            if ($validator->isValid()) {
+                $roleUpdate = new Role();
+                $roleUpdate->setTitle($_POST['title']);
+
+                $roleRepository->updateRole($roleUpdate, $id);
+            }
+            $errors = $validator->getErrors();
+
+            if (!empty($errors)) {
+                echo $this->twig->render('admin/role/edit-role.html.twig', [
+                    'errors' => $errors,
+                    'role' => $role
+                ]);
+            } else {
+                header('Location: /admin/list-role');
+            }
         }
     }
 
     #[Route(path: "/admin/delete-role/{id}", name: "delete-role")]
     public function deleteRole(RoleRepository $roleRepository,int $id)
     {
-        $roleRepository->deleteRole($id);
-        header('Location: /admin/list-role');
+        if (!isset($_SESSION['userRole'])) {
+            header('Location: /form-login');
+        }
+        if ($_SESSION['userRole'] !== 'Admin') {
+            echo $this->twig->render('/access.html.twig');
+        } else {
+            $roleRepository->deleteRole($id);
+            header('Location: /admin/list-role');
+        }
     }
 }
